c8458cc49e8570272e5a8c7528e1182a39b4084c
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-06 20:33:45 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
