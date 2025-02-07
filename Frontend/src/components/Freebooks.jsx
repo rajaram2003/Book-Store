0d98eba52000470508ef8a9d928dd2c6e8148bf8
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-07 19:29:15 +0530

    change

--- Frontend/src/components/Freebooks.jsx
+++ Frontend/src/components/Freebooks.jsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 import list from "../../public/list.json"
+import Cards from "./Cards";
 
 const Freebooks = () => {
   const filterData = list.filter((Data) => Data.category === "Free");
@@ -29,14 +30,16 @@ const Freebooks = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
+          initialSlide: 2,
+          dots: true
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          dots: true
         }
       }
     ]
@@ -53,30 +56,9 @@ const Freebooks = () => {
 
     <div>
     <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
+    {filterData.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
       </Slider>
     </div>
     </div>
