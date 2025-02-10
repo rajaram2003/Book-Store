5e5c5c95eb8f77a534287007ddfbdc9b83c8fa4e
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-10 20:33:23 +0530

    change

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,31 +1,48 @@
+import { useEffect, useState } from "react";
 import Cards from "./Cards";
-import list from "../../public/list.json";
+import axios from "axios";
 import { Link } from "react-router-dom";
-
-const Course = () => {
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl md:text-4xl">Expand Your Knowledge, One Book at a <span className="text-pink-500">Time!</span></h1>
-          <p className="mt-12">Welcome to Bookstore! Explore bestsellers, classics, and inspiring reads. Find your next favorite book with ease, enjoy great deals, and embark on a journey of knowledge and imagination.</p>
+          <h1 className="text-2xl  md:text-4xl">
+            Welcome to Our Book Collection{" "}
+            <span className="text-pink-500">📚 Explore & Enjoy!</span>
+          </h1>
+          <p className="mt-12">
+          Dive into a world of knowledge and imagination with our curated collection of books.  
+          Whether you are a fiction lover, a tech enthusiast, or an aspiring entrepreneur,  
+          we have something for everyone. Start your journey today!
+          </p>
           <Link to="/">
-          <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
-            Back
-          </button>
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
           </Link>
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
-          {
-            list.map((item) => (
-              <Cards key={item.id} item={item}/>
-            ))
-          }
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Course
-
+export default Course;
