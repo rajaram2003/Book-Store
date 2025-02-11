b1d63a2a75a204cbbd1387d2914925f38f076dd2
rajaram2003 <rajarambiswal123@gmail.com>
2025-02-11 20:06:44 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import bookRoute from "./route/book.route.js"
+import userRoute from "./route/user.route.js"
 
 const app = express();
 
@@ -30,7 +31,8 @@ try {
 
 //Define routes
 
-app.use("/book",bookRoute)
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
